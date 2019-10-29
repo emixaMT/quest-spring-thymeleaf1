2e34411b8cc9884295cb67388fedac97b2681311
Maxime Thonneau <thonneaum@gmail.com>
2019-10-29 17:10:38 +0100

    Spring 03

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,13 +1,27 @@
 package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
-
+    
     @GetMapping("/doctor")
-    public String doctor() {
-        return "doctor";
+    @RequestMapping(method = RequestMethod.GET)
+    public String getDoctor(@RequestParam String name, @RequestParam String number, Model model) {
+        
+        model.addAttribute("name",name);
+        model.addAttribute("number",number);
+        if(name=="") {
+        	model.addAttribute("name","John Smith");
+        }
+        if(number=="") {
+        	model.addAttribute("number","0");
+        }
+    	return "doctor";
     }
 }
